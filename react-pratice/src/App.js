82d53744620d17a88ff6ccaa2e5f2bea490bb6c6
Jinhua Yang <jyang496@uwo.ca>
2023-06-21 18:44:27 -0400

    rgb squares with hover effect

--- react-pratice/src/App.js
+++ react-pratice/src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import {useState} from "react";
+
+
+const squareArray = []
+
+const Square = ({i}) =>{
+    const [isMo, setMO] = useState(false)
+
+
+    const color = `rgb(${i[0]}, ${i[1]}, ${i[2]})`;
+    const bgColor = {
+        background: color
+    }
+    const hoverColor = {
+        background: 'black'
+    }
+    return(
+        // isHovered is always false?
+        <div className="square"
+             onMouseEnter={() => setMO(true)}
+             onMouseLeave={() => setMO(false)}
+             style={isMo ? hoverColor : bgColor}>
+        </div>
+    )
+}
+
+const createArray = () => {
+    let delta = 255/750
+
+    for (let i = 0; i < 750; i++){
+        let currentColor = [255,0,0]
+        currentColor[0] = 255 - delta * (i+1)
+        currentColor[1] = delta * (i+1)
+        squareArray.push(currentColor)
+    }
+    for (let i = 0; i < 750; i++){
+        let currentColor = [0,255,0]
+        currentColor[1] = 255 - delta * (i+1)
+        currentColor[2] = delta * (i+1)
+        squareArray.push(currentColor)
+    }
+}
+
 
 function App() {
+    createArray()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="squareContainer">
+          {squareArray.map((item, index) => <Square key={index} i={item}/>)}
+      </div>
   );
 }
 
